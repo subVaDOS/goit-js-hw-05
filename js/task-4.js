6a04090a3cfa1161b3f8a1979459598d9c04a359
Manokha Vadim <manokha.vadim@gmail.com>
2025-04-05 12:08:35 +0300

    moddified js

--- js/task-4.js
+++ js/task-4.js
@@ -23,3 +23,35 @@
 
 // profile.updatePlayTime(20);
 // console.log(profile.getInfo()); // "Marco has 320 active hours!"
+// function registerGuest(name, callback) {
+//   console.log(`Registering ${name}!`);
+//   callback(name);
+// }
+
+// // Передаємо інлайн-функцію greet у якості колбека
+// registerGuest('Mango', function greet(name) {
+//   console.log(`Welcome ${name}!`);
+// });
+
+// // Передаємо інлайн-функцію notify у якості колбека
+// registerGuest('Poly', function notify(name) {
+//   console.log(`Dear ${name}, your room will be ready in 30 minutes`);
+// });
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
+
+  numbers.forEach(number => {
+    if (number > value) {
+      filteredNumbers.push(number);
+    }
+  });
+
+  return filteredNumbers;
+}
+
+// Перевірка:
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]

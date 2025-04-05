69331b8d74245ced8c1a963f91df5f6c76a43d73
Manokha Vadim <manokha.vadim@gmail.com>
2025-04-05 12:42:58 +0300

    moddified js

--- js/task-4.js
+++ js/task-4.js
@@ -65,3 +65,16 @@
 // const arrowAdd = (a, b, c) => {
 //   return a + b + c;
 // };
+const calculateTotalPrice = orderedItems => {
+  let totalPrice = 0;
+
+  orderedItems.forEach(item => {
+    totalPrice += item;
+  });
+
+  return totalPrice;
+};
+
+console.log(calculateTotalPrice([12, 85, 37, 4])); // 138
+console.log(calculateTotalPrice([164, 48, 291])); // 503
+console.log(calculateTotalPrice([412, 371, 94, 63, 176])); // 1116

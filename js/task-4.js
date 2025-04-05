fb1b0a5f3c101b0402a5f3dfa1b6d7986d3c471a
Manokha Vadim <manokha.vadim@gmail.com>
2025-04-05 19:14:37 +0300

    moddified js

--- js/task-4.js
+++ js/task-4.js
@@ -88,20 +88,20 @@
 // dirtyMultiply(numbers, 2);
 // // Відбулася мутація вихідних даних - масиву numbers
 // console.log(numbers); // [2, 4, 6, 8, 10]
-const pureMultiply = (array, value) => {
-  const newArray = [];
+// const pureMultiply = (array, value) => {
+//   const newArray = [];
 
-  array.forEach(element => {
-    newArray.push(element * value);
-  });
+//   array.forEach(element => {
+//     newArray.push(element * value);
+//   });
 
-  return newArray;
-};
+//   return newArray;
+// };
 
-const numbers = [1, 2, 3, 4, 5];
-const doubledNumbers = pureMultiply(numbers, 2);
+// const numbers = [1, 2, 3, 4, 5];
+// const doubledNumbers = pureMultiply(numbers, 2);
 
-// Мутація вихідних даних не відбулася
-console.log(numbers); // [1, 2, 3, 4, 5]
-// Функція повернула новий масив зі зміненими даними
-console.log(doubledNumbers); // [2, 4, 6, 8, 10]
+// // Мутація вихідних даних не відбулася
+// console.log(numbers); // [1, 2, 3, 4, 5]
+// // Функція повернула новий масив зі зміненими даними
+// console.log(doubledNumbers); // [2, 4, 6, 8, 10]

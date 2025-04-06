2e2881f4211da7aaad55362cc4f7f85ed88ada3b
Manokha Vadim <manokha.vadim@gmail.com>
2025-04-06 21:25:06 +0300

    modified js 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,313 +1,45 @@
-// const profile = {
-//   username: 'Jacob',
-//   playTime: 300,
+const getTotalBalanceByGender = (users, gender) =>
+  users
+    .filter(user => user.gender === gender)
+    .reduce((total, user) => total + user.balance, 0);
 
-//   changeUsername(newName) {
-//     this.username = newName;
-//   },
+const clients = [
+  {
+    name: 'Moore Hensley',
+    gender: 'male',
+    balance: 2811,
+  },
+  {
+    name: 'Sharlene Bush',
+    gender: 'female',
+    balance: 3821,
+  },
+  {
+    name: 'Ross Vazquez',
+    gender: 'male',
+    balance: 3793,
+  },
+  {
+    name: 'Elma Head',
+    gender: 'female',
+    balance: 2278,
+  },
+  {
+    name: 'Carey Barr',
+    gender: 'male',
+    balance: 3951,
+  },
+  {
+    name: 'Blackburn Dotson',
+    gender: 'male',
+    balance: 1498,
+  },
+  {
+    name: 'Sheree Anthony',
+    gender: 'female',
+    balance: 2764,
+  },
+];
 
-//   updatePlayTime(hours) {
-//     this.playTime += hours;
-//   },
-
-//   getInfo() {
-//     return `${this.username} has ${this.playTime} active hours!`;
-//   },
-// };
-
-// // Перевірка коректності роботи об'єкта
-// console.log(profile.getInfo()); // "Jacob has 300 active hours!"
-
-// profile.changeUsername('Marco');
-// console.log(profile.getInfo()); // "Marco has 300 active hours!"
-
-// profile.updatePlayTime(20);
-// console.log(profile.getInfo()); // "Marco has 320 active hours!"
-// function registerGuest(name, callback) {
-//   console.log(`Registering ${name}!`);
-//   callback(name);
-// }
-
-// // Передаємо інлайн-функцію greet у якості колбека
-// registerGuest('Mango', function greet(name) {
-//   console.log(`Welcome ${name}!`);
-// });
-
-// // Передаємо інлайн-функцію notify у якості колбека
-// registerGuest('Poly', function notify(name) {
-//   console.log(`Dear ${name}, your room will be ready in 30 minutes`);
-// });
-// function filterArray(numbers, value) {
-//   const filteredNumbers = [];
-
-//   numbers.forEach(number => {
-//     if (number > value) {
-//       filteredNumbers.push(number);
-//     }
-//   });
-
-//   return filteredNumbers;
-// }
-
-// // Перевірка:
-// console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-// console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-// console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-// console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-// console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
-// Звичайне оголошення функції
-
-// function classicAdd(a, b, c) {
-//   return a + b + c;
-// }
-
-// // Те саме стрілочною функцією
-// const arrowAdd = (a, b, c) => {
-//   return a + b + c;
-// };
-// const calculateTotalPrice = orderedItems => {
-//   let totalPrice = 0;
-
-//   orderedItems.forEach(item => {
-//     totalPrice += item;
-//   });
-
-//   return totalPrice;
-// };
-
-// console.log(calculateTotalPrice([12, 85, 37, 4])); // 138
-// console.log(calculateTotalPrice([164, 48, 291])); // 503
-// console.log(calculateTotalPrice([412, 371, 94, 63, 176])); // 1116
-// const dirtyMultiply = (array, value) => {
-//   for (let i = 0; i < array.length; i += 1) {
-//     array[i] = array[i] * value;
-//   }
-// };
-
-// const numbers = [1, 2, 3, 4, 5];
-// dirtyMultiply(numbers, 2);
-// // Відбулася мутація вихідних даних - масиву numbers
-// console.log(numbers); // [2, 4, 6, 8, 10]
-// const pureMultiply = (array, value) => {
-//   const newArray = [];
-
-//   array.forEach(element => {
-//     newArray.push(element * value);
-//   });
-
-//   return newArray;
-// };
-
-// const numbers = [1, 2, 3, 4, 5];
-// const doubledNumbers = pureMultiply(numbers, 2);
-
-// // Мутація вихідних даних не відбулася
-// console.log(numbers); // [1, 2, 3, 4, 5]
-// // Функція повернула новий масив зі зміненими даними
-// console.log(doubledNumbers); // [2, 4, 6, 8, 10]
-
-// const planets = ['Earth', 'Mars', 'Venus', 'Jupiter'];
-
-// const planetsInUpperCase = planets.map(planet => planet.toUpperCase());
-// console.log(planetsInUpperCase); // ["EARTH", "MARS", "VENUS", "JUPITER"]
-
-// const planetsInLowerCase = planets.map(planet => planet.toLowerCase());
-// console.log(planetsInLowerCase); // ["earth", "mars", "venus", "jupiter"]
-
-// // Оригінальний масив не змінився
-// console.log(planets); // ["Earth", "Mars", "Venus", "Jupiter"]
-
-// const students = [
-//   { name: 'Mango', score: 83 },
-//   { name: 'Poly', score: 59 },
-//   { name: 'Ajax', score: 37 },
-//   { name: 'Kiwi', score: 94 },
-//   { name: 'Houston', score: 64 },
-// ];
-
-// const names = students.map(student => student.name);
-// console.log(names); // ["Mango", "Poly", "Ajax", "Kiwi", "Houston"]
-
-// const students = [
-//   { name: 'Mango', courses: ['mathematics', 'physics'] },
-//   { name: 'Poly', courses: ['science', 'mathematics'] },
-//   { name: 'Kiwi', courses: ['physics', 'biology'] },
-// ];
-
-// const mappedCourses = students.map(student => student.courses);
-// console.log(mappedCourses); // [["mathematics", "physics"], ["science", "mathematics"], ["physics", "biology"]]
-
-// const flattenedCourses = students.flatMap(student => student.courses);
-// console.log(flattenedCourses); // ["mathematics", "physics", "science", "mathematics", "physics", "biology"];
-
-// const LOW_SCORE = 50;
-// const HIGH_SCORE = 80;
-// const students = [
-//   { name: 'Mango', score: 83 },
-//   { name: 'Poly', score: 59 },
-//   { name: 'Ajax', score: 37 },
-//   { name: 'Kiwi', score: 94 },
-//   { name: 'Houston', score: 64 },
-//   { name: 'Chelsy', score: 85 },
-//   { name: 'Boris', score: 45 },
-//   { name: 'Lola', score: 72 },
-//   { name: 'Mia', score: 29 },
-//   { name: 'Tom', score: 91 },
-//   { name: 'Jerry', score: 66 },
-//   { name: 'Oscar', score: 52 },
-//   { name: 'Liam', score: 78 },
-//   { name: 'Emma', score: 49 },
-//   { name: 'Olivia', score: 88 },
-//   { name: 'Noah', score: 73 },
-//   { name: 'Ava', score: 39 },
-// ];
-
-// const best = students.filter(student => student.score >= HIGH_SCORE);
-// console.log(best); // Масив об'єктів з іменами Mango і Kiwi
-
-// const worst = students.filter(student => student.score < LOW_SCORE);
-// console.log(worst); // Масив з одним об'єктом Ajax
-
-// const average = students.filter(
-//   student => student.score >= LOW_SCORE && student.score < HIGH_SCORE
-// );
-// console.log(average); // Масив об'єктів з іменами Poly і Houston
-
-// const colorPickerOptions = [
-//   { label: 'red', color: '#F44336' },
-//   { label: 'green', color: '#4CAF50' },
-//   { label: 'blue', color: '#2196F3' },
-//   { label: 'pink', color: '#E91E63' },
-//   { label: 'indigo', color: '#3F51B5' },
-// ];
-
-// colorPickerOptions.find(option => option.label === 'blue'); // { label: "blue", color: "#2196F3" }
-// colorPickerOptions.find(option => option.label === 'pink'); // { label: "pink", color: "#E91E63" }
-// colorPickerOptions.find(option => option.label === 'white'); // undefined
-// colorPickerOptions.find(option => option.label === 'red'); // { label: "red", color: "#F44336" }
-// colorPickerOptions.find(option => option.label === 'green'); // { label: "green", color: "#4CAF50" }
-
-// console.log(colorPickerOptions.find(option => option.label === 'blue')); // { label: "blue", color: "#2196F3" }
-// console.log(colorPickerOptions.find(option => option.label === 'pink')); // { label: "pink", color: "#E91E63" }
-// console.log(colorPickerOptions.find(option => option.label === 'white')); // undefined
-// console.log(colorPickerOptions.find(option => option.label === 'red')); // { label: "red", color: "#F44336" }
-// console.log(colorPickerOptions.find(option => option.label === 'green')); // { label: "green", color: "#4CAF50" }
-
-// const products = [
-//   { name: 'apple', quantity: 1 },
-//   { name: 'orange', quantity: 2 },
-//   { name: 'plum', quantity: 3 },
-// ];
-
-// const hasEveryProduct = products.every(product => product.quantity > 0);
-// console.log(hasEveryProduct); // false
-
-// array.some((element, index, array) => {
-//   // Тіло колбек-функції
-// });
-
-// // Чи є хоча б один елемент, що більший або дорівнює нулю? - так
-// [1, 2, 3, 4, 5].some(value => value >= 0); // true
-
-// // Чи є хоча б один елемент, що більший або дорівнює нулю? - так
-// [-7, -20, 3, -10, -14].some(value => value >= 0); // true
-
-// // Чи є хоча б один елемент, що менший від нуля? - ні
-// [1, 2, 3, 4, 5].some(value => value < 0); // false
-
-// // Чи є хоча б один елемент, що менший від нуля? - так
-// [1, 2, 3, -10, 4, 5].some(value => value < 0); // true
-
-// const total = [2, 7, 3].reduce((previousValue, number) => {
-//   return previousValue + number;
-// }, 0);
-
-// console.log(total); // 12
-
-// const students = [
-//   { name: 'Mango', score: 83 },
-//   { name: 'Poly', score: 59 },
-//   { name: 'Ajax', score: 37 },
-//   { name: 'Kiwi', score: 94 },
-//   { name: 'Houston', score: 64 },
-// ];
-
-// // Назва акумулятора може бути довільною, це просто параметр функції
-// const totalScore = students.reduce((total, student) => {
-//   return total + student.score;
-//   console.log(averageScore); // 67.4
-// }, 0);
-
-// const averageScore = totalScore / students.length;
-// console.log(averageScore); // 67.4
-// // console.log(averageScore); // 67.4
-
-// const players = [
-//   { name: 'Mango', playtime: 1270, gamesPlayed: 4 },
-//   { name: 'Poly', playtime: 469, gamesPlayed: 2 },
-//   { name: 'Ajax', playtime: 690, gamesPlayed: 3 },
-//   { name: 'Kiwi', playtime: 241, gamesPlayed: 1 },
-// ];
-
-// const totalAveragePlaytimePerGame = players.reduce((acc, player) => {
-//   return acc + player.playtime / player.gamesPlayed;
-// }, 0);
-// console.log(totalAveragePlaytimePerGame); // 1023
-
-// const calculateTotalBalance = users => {
-//   return users.reduce((total, user) => total + user.balance, 0);
-// };
-
-// const users = [
-//   { name: 'Moore Hensley', balance: 2811 },
-//   { name: 'Sharlene Bush', balance: 3821 },
-//   { name: 'Ross Vazquez', balance: 3793 },
-//   { name: 'Elma Head', balance: 2278 },
-//   { name: 'Carey Barr', balance: 3951 },
-//   { name: 'Blackburn Dotson', balance: 1498 },
-//   { name: 'Sheree Anthony', balance: 2764 },
-// ];
-
-// console.log(calculateTotalBalance(users)); // 20916
-
-// const clients = [
-//   {
-//     name: 'Moore Hensley',
-//     gender: 'male',
-//     balance: 2811,
-//   },
-//   {
-//     name: 'Sharlene Bush',
-//     gender: 'female',
-//     balance: 3821,
-//   },
-//   {
-//     name: 'Ross Vazquez',
-//     gender: 'male',
-//     balance: 3793,
-//   },
-//   {
-//     name: 'Elma Head',
-//     gender: 'female',
-//     balance: 2278,
-//   },
-//   {
-//     name: 'Carey Barr',
-//     gender: 'male',
-//     balance: 3951,
-//   },
-//   {
-//     name: 'Blackburn Dotson',
-//     gender: 'male',
-//     balance: 1498,
-//   },
-//   {
-//     name: 'Sheree Anthony',
-//     gender: 'female',
-//     balance: 2764,
-//   },
-// ];
-
-// console.log(getTotalBalanceByGender(clients, 'male')); // 12053
-
-// console.log(getTotalBalanceByGender(clients, 'female')); // 8863
+console.log(getTotalBalanceByGender(clients, 'male')); // 12053
+console.log(getTotalBalanceByGender(clients, 'female')); // 8863
